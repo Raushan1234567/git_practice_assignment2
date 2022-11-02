7887d67b8d660b6107dcea616838d8c83656893a
Amit0841 <aroy16063@gmail.com>
2022-11-02 17:45:01 +0530

    Check it

--- prime_problem.js
+++ prime_problem.js
@@ -1,14 +1,13 @@
 let number=13;
-let factor=0;
+let count=0;
 for(let i=1;i<=num;i++)
 {
-   if(number%i==0)
-{
-    factor++;
-}}
-if(factor==2)
-{
+   if(number%i==0) {
+    count++;
+  }
+ }
+if(count==2) {
 console.log("prime number");
-}
-else 
+} else {
 console.log("not prime");
+}
